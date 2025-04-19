0be2c206e3bc6d1d6cd8211c98951fccf8702d4f
jbiloso <jbiloso@up.edu.ph>
2025-04-20 01:33:08 +0800

    Exercise 3.8: Phonebook backend step 8 - Configured morgan to show data send in HTTP POST requests

--- phonebook/index.js
+++ phonebook/index.js
@@ -3,7 +3,14 @@ const express = require('express')
 const app = express() 
 const morgan = require('morgan')
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('details',  (req)=> {
+    return req.method === 'POST' ? JSON.stringify(req.body) : ''
+    }
+)
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :details'))
+
+
 let persons = [
     {
         "id":"1",
